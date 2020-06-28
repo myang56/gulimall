a0d277a529c28f6e27c00a2ece67e07a15e37b4f
miao yang <miao.yang10@t-mobile.com>
2020-06-27 23:25:32 -0700

    set up modules

--- gulimall-product/src/main/java/com/ym/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/ym/gulimall/product/GulimallProductApplication.java
@@ -1,8 +1,32 @@
 package com.ym.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ *  1. 整合MyBatis-Plus
+ *     1. 导入依赖
+ *       <dependency>
+ *             <groupId>com.baomidou</groupId>
+ *             <artifactId>mybatis-plus-boot-starter</artifactId>
+ *             <version>3.3.2</version>
+ *         </dependency>
+ *      2. 配置
+ *         1. 配置数据源
+ *             1.导入数据库驱动
+ *             2. 在application.yml 配置数据相关信息
+ *         2. 配置Mybatis-plus
+ *            1. 使用@MapperScan
+ *            2. 告诉mybatis-plus， sql映射文件
+ *  2. 逻辑删除
+ *  1. 配置全局的逻辑删除规则 （省略）
+ *  2. 配置逻辑删除的组件bean (3.1 以上省略）
+ *  3. 加上逻辑删除注解
+ */
+@EnableDiscoveryClient
+@MapperScan("com.ym.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
--- gulimall-product/src/test/java/com/ym/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/ym/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,67 @@
-package com.ym.gulimall.product;
+    package com.ym.gulimall.product;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class GulimallProductApplicationTests {
+    import com.aliyun.oss.OSSClient;
+    import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+    import com.ym.gulimall.product.entity.BrandEntity;
+    import com.ym.gulimall.product.service.BrandService;
+    import org.junit.jupiter.api.Test;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.boot.test.context.SpringBootTest;
 
-    @Test
-    void contextLoads() {
+    import java.io.FileInputStream;
+    import java.io.FileNotFoundException;
+    import java.io.InputStream;
+    import java.util.List;
+
+    /**
+     *  1. 引入oss starter
+     *  2. 配置key endpointer等相关信息
+     *  3. 使用oss client进行相关操作
+     */
+
+    @SpringBootTest
+    class GulimallProductApplicationTests {
+
+        @Autowired
+        BrandService brandService;
+
+        @Autowired
+        OSSClient ossClient;
+
+        @Test
+        public void testUpload() throws FileNotFoundException {
+//
+//            // Endpoint以杭州为例，其它Region请按实际情况填写。
+//            String endpoint = "oss-us-west-1.aliyuncs.com";
+//            // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
+//            String accessKeyId = "LTAI4G3CXbTYNYksN67ygbSA";
+//            String accessKeySecret = "dJc1XzbudyDi7GcWChjNxkz6zDBVtM";
+
+//           // 创建OSSClient实例。
+//            OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+            // 上传文件流。
+            InputStream inputStream = new FileInputStream("/Users/miao/desktop/uwm1.jpeg");
+            ossClient.putObject("gulimall-wa", "uwm1.jpeg", inputStream);
+
+            // 关闭OSSClient。
+            ossClient.shutdown();
+            System.out.println("upload success!");
+        }
+
+        @Test
+        void contextLoads() {
+    //        BrandEntity brandEntity = new BrandEntity();
+    //        brandEntity.setName("huawei");
+    //        brandService.save(brandEntity);
+            List<BrandEntity> list =  brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1));
+            list.forEach((item) -> {
+                System.out.println(item);
+            });
+
+
+            System.out.println("save success");
+        }
+
     }
-
-}
