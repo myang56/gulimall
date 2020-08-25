19dc33ff7ca81791b0024d3c1c404a47d58f19fe
miao yang <miao.yang10@t-mobile.com>
2020-08-24 17:04:40 -0700

    initial rabbitmq setup

--- gulimall-order/src/main/java/com/ym/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/ym/gulimall/order/GulimallOrderApplication.java
@@ -1,9 +1,20 @@
 package com.ym.gulimall.order;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 使用rabbitmq
+ * 1. 引入amqp， RabbitAutoConfiguration自动生效
+ * 2. 给容器中自动配置了
+ *     RabbitTemplate, AmqpAdmin, CachingConnectionFactory, RabbitMessagingTemplate
+ * 3. 给配置文件中配置spring.rabbitmq信息
+ * 4. @EnableRabbit
+ */
+
+@EnableRabbit
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallOrderApplication {
--- gulimall-order/src/test/java/com/ym/gulimall/order/GulimallOrderApplicationTests.java
+++ gulimall-order/src/test/java/com/ym/gulimall/order/GulimallOrderApplicationTests.java
@@ -1,13 +1,54 @@
 package com.ym.gulimall.order;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class GulimallOrderApplicationTests {
 
+    /**
+     * 1. how to create exchanges,  queue, binding
+     *    use AmqpAdmin
+     * how to send/read message
+     */
+    @Autowired
+    AmqpAdmin amqpAdmin;
+
+    @Test
+    void createExchange() {
+
+        // DirectExchange(String name, boolean durable, boolean autoDelete)
+        DirectExchange directExchange = new DirectExchange("hello-java-exchange", true, false);
+        amqpAdmin.declareExchange(directExchange);
+        log.info("Exchange[{}]创建成功", directExchange.getName());
+    }
+
+    @Test
+    void createQueue() {
+
+        // Queue(String name, boolean durable, boolean exclusive, boolean autoDelete, @Nullable Map<String, Object> arguments
+        Queue queue = new Queue("hello-java-queue", true, false, false);
+        amqpAdmin.declareQueue(queue);
+        log.info("Exchange[{}]创建成功", queue.getName());
+    }
+
+    @Test
+    void createBinding() {
+        // Binding(String destination, Binding.DestinationType destinationType, String exchange, String routingKey, @Nullable Map<String, Object> arguments)
+        Binding binding = new Binding("hello-java-queue", Binding.DestinationType.QUEUE, "hello-java-exchange", "hello-java", null);
+        amqpAdmin.declareBinding(binding);
+    }
+
     @Test
     void contextLoads() {
+
     }
 
 }
